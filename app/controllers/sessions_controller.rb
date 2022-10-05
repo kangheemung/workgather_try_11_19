2662a32f502ab77a728ccdafa10756a2d0722e73
EC2 Default User <ec2-user@ip-172-31-12-57.ap-northeast-1.compute.internal>
2022-10-05 07:31:17 +0000

    1005

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    #p "=============="
+    #p params
+    #p "=============="
+     user=User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      # session[:user_id] = user.id
+      login(user)
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'invalid email/password combination'
+      render 'new'
+    end
+  end
+  def destroy
+     session[:user_id] = nil
+     redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
   #top
   root 'home#top'
+
   get 'home/select_position'=>'home#select_position'
   get 'home/select_position_E'=>'home#select_position_E'
   get 'home/select_position_K'=>'home#select_position_K'
   get 'home/about'=>'home#about'
 
-  get 'posts#index'=>'posts#index'
+  get 'posts/index'=>'posts#index'
   get 'posts/show' => 'posts#show'
   get 'posts/new' => 'posts#new'
- 
+
+  #login
+  get 'sessions/new'=>'sessions#new',as: 'login'
+  post 'sessions/create'=>'sessions#create',as: 'sessions'
+  delete 'logout' =>'sessions#destroy'
+
+  #signup
+  get 'users/participate_new' => 'users#participate_new', as: 'signup'
+  get 'users/workshop_Organizer_new'=>'users#workshop_Organizer_new'
+  post 'users/create'=>'users#create',as: 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
