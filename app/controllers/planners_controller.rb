d2756812954517aa2f9876b922e1e10825e5072e
EC2 Default User <ec2-user@ip-172-31-12-57.ap-northeast-1.compute.internal>
2022-10-16 05:26:23 +0000

    何でもいい2

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -2,17 +2,17 @@ class PlannersController < ApplicationController
   def index
     @planners= Planner.all
   end
-  d  def new
-  @planner=Planner.new
-end
+  def new
+    @planner=Planner.new
+  end
 def create
   planner = Planner.new(planner_params)
    if planner.save
-    log_in(planner)
-    #  session[:planner_id]=planner.id
-    flash[:notice]="ユーザー登録が完了しました。"
-    redirect_to("/planners/login_form")
-  else
+      log_in(planner)
+      #  session[:planner_id]=planner.id
+      flash[:notice]="ユーザー登録が完了しました。"
+      redirect_to("/planners/login_form")
+    else
     render :new
   end
 end
