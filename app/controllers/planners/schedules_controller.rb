aa31e844c69b4fd667118137cea4d8d1ac23ec7a
EC2 Default User <ec2-user@ip-172-31-12-57.ap-northeast-1.compute.internal>
2023-01-06 13:59:41 +0000

    スケジュールのshow page作成上手くいかない。

--- app/controllers/planners/schedules_controller.rb
+++ app/controllers/planners/schedules_controller.rb
@@ -16,24 +16,33 @@ class Planners::SchedulesController < ApplicationController
       p schedule.errors.full_messages
       p"==========="
     if schedule.save
-      redirect_to planners_schedules_show_path
+      redirect_to planners_schedules_show_path(id: params[:workshop_id])
     else  
       render "new"
     end
   end
-
   def show
     @schedule =Schedule.find_by(params[:id])
-    @planner = Planner.find_by(id:params[:planner_id])
+    @planner = Planner.find_by(params[:planner_id])
     @workshop =Workshop.find_by(id: params[:workshop_id])
   
     
   end
-
+  def  update
+    @schedule=Schedule.find(params[:id]) #値を取得する
+    if  @schedule.update(schedule_params)  #workshop_paramsの内容を上書きする。
+      
+        flash[:notice]="プランナー情報を更新しました。"
+        redirect_to planners_schedules_show_path(@schedule.workshop_id),data: {"turbolinks" => false}
+    else
+      render :edit
+    end
+  end
   def edit
+    @schedule =Schedule.find_by(params[:id])
   end
   private
   def schedule_params
-    params.require(:schedule).permit(:planner_id,:workshop_id,:title,:content,:day,:time,:start_date,:end_date)
+    params.require(:schedule).permit(:title,:content,:start_time,:end_time,:workshop_id,:start_date,:planenr_id)
   end 
 end
--- app/controllers/users/reservations_controller.rb
+++ app/controllers/users/reservations_controller.rb
@@ -7,19 +7,20 @@ class Users::ReservationsController < ApplicationController
     @reservation = Reservation.new
   end
   def create
-    reservation= current_user.reservations.build(reservation_params) 
+    reservation=reservation.new(reservation_params) 
     p"============"
     p @reservation.errors.full_messages
     p"============"
-    if reservation.save!
-      redirect_to users_reservation_index_path 
+    if reservation.save
+      redirect_to users_reservation_show_path 
     else
       render :new
     end
   end    
   def show
+    @user=User.find_by(params[:id])
     @workshop=Workshop.find_by(id:params[:workhshop_id])
-    @reservation=Reservation.find_by(params[:reservation_id])
+    @reservation=Reservation.find_by(params[:id])
   end
     def edit
       @reservation=Reservation.find_by(params[:id])
@@ -29,13 +30,13 @@ class Users::ReservationsController < ApplicationController
       if @reservation.update(reservation_params)  #workshop_paramsの内容を上書きする。
         
           flash[:notice]="プランナー情報を更新しました。"
-          redirect_tou users_reservations_show_path(@reservation.workshop_id),data: {"turbolinks" => false}
+          redirect_to users_reservations_show_path(@reservation.workshop_id),data: {"turbolinks" => false}
       else
         render :edit
       end
     end
     private
     def reservation_params
-      params.require(:reservation).permit(:day,:time,:workshop_id,:user_id,:start_time)
+      params.require(:reservation).permit(:title,:content,:start_time,:end_time,:workshop_id,:start_date,:user_id)
     end 
   end
