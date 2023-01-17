b4fdc7d77eacea554aa6a11e35907dbbf358bd58
EC2 Default User <ec2-user@ip-172-31-12-57.ap-northeast-1.compute.internal>
2023-01-17 14:29:15 +0000

    予約 planner index改善した。

--- app/controllers/planners/reservations_controller.rb
+++ app/controllers/planners/reservations_controller.rb
@@ -1,6 +1,6 @@
 class Planners::ReservationsController < ApplicationController
   def index
     @workshops=current_planner.workshops.all
-    
+    @workshop=Workshop.find_by(params[:workshop_id])
   end
 end
