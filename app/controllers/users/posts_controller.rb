433c01906f4223c9df7b7eafae24665bcfb53c5b
EC2 Default User <ec2-user@ip-10-0-0-39.ap-northeast-1.compute.internal>
2023-11-22 15:51:40 +0000

    post_new_done

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,22 +1,43 @@
 class Users::PostsController < ApplicationController
   include  UserSessionsHelper
-    def create
-      @post=current_user.posts.build(post_params)
-         @post.save!
-          redirect_to user_workshops_index_path
-      
-        
-       
+  def new
+    @user = current_user
+    
+    @reservation = Reservation.find_by(workshop_id: params[:workshop_id], user_id: @user.id)
+    
+    if @reservation.nil?
+      flash[:alert] = "You cannot review a workshop you didn't participate in."
+      redirect_to users_workshops_index_path
+      return
     end
+    # Find the workshop using the workshop_id from the params
+    @workshop = Workshop.find(params[:workshop_id])
+    
+    # Initialize a new post with just the workshop_id and user_id set
+    @post = Post.new(workshop_id: @workshop.id, user_id: @user.id)
+
+    # Optional: Get all posts for the workshop if needed to display elsewhere
+    @posts = @workshop.posts
+  end
+  def create
+    @post = current_user.posts.build(post_params)
+    @post.save
+      redirect_to users_workshops_index_path # Ensure this is also correctly defined route
+    
+    
+  end
+
     def edit
-      @post=Post.find_by(params[:id])
+      @post = Post.find(params[:post_id])
     end
     def update
-      @post=Post.find_by(params[:id])
+      @post = Post.find(params[:post_id])
+
       if @post.update(post_params)
+        flash[:notice] = "参加者レビュー情報を更新しました。"
+        redirect_to posts_show_path(@post.id)
           #  session[:planner_id]=planner.id
           flash[:notice]="参加者レビュー情報を更新しました。"
-          redirect_to users_workshops_show_path(@post.workshop_id)
       else
         render :edit
       end
--- config/routes.rb
+++ config/routes.rb
@@ -9,28 +9,15 @@ Rails.application.routes.draw do
  get 'homes/sign_in'=>'homes#sign_in',as:'home_sign_in'
  get 'homes/sign_up'=>'homes#sign_up',as:'home_sign_up'
  get 'homes/about'=>'homes#about'
-  namespace :users do
-      #参加者ログイン
-      get '/login_form' => 'user_sessions#new',as:'login'
-      post '/login' => 'user_sessions#create',as:'login_create'
-      delete '/logout' => 'user_sessions#destroy',as:'logout'
-  end
-  namespace :users do
-    #参加者会員登録
-    get '/index' => 'users#index',as:'index'
-    get '/new'=>'users#new',as:'signup'
-    post '/create'=>'users#create',as:'create'
-    get '/:user_id/edit' => 'users#edit',as:'edit'
-    patch '/:user_id/edit'=>'users#update',as:'update'
-    get '/:user_id'=>'users#show',as:'show'
-  end  
+  
+ 
+
+ 
   namespace :planners do
     #開催者ログイン
     get '/login_form' => 'planner_sessions#new',as:'login'
     post '/login' => 'planner_sessions#create',as:'login_create'
     delete '/logout' => 'planner_sessions#destroy',as:'logout'
-  end
-  namespace :planners do
     #開催者会員登録
     get '/new'=> 'planners#new',as:'new'
     get '/index'=>'planners#index',as:'index'
@@ -39,9 +26,7 @@ Rails.application.routes.draw do
     get '/:planner_id/edit' => 'planners#edit',as:'edit'
     post '/:planner_id/edit' => 'planners#update',as:'update'
     get '/:planner_id' => 'planners#show',as:'show'
-  end
-       #planner_workshops投稿
-  namespace :planners do
+      #planner_workshops投稿
     get '/workshops/index'=>'workshops#index',as:'workshops_index'
     post '/workshops/create/:planner_id'=>'workshops#create',as:'workshops_create'
     get '/workshops/:planner_id/new'=>'workshops#new',as:'workshops_new'
@@ -49,50 +34,54 @@ Rails.application.routes.draw do
     patch '/workshops/:planner_id/:workshop_id/edit' => 'workshops#update',as:'workshops_update'
     delete '/workshops/:planner_id/:workshop_id/edit' => 'workshops#destroy',as:'workshops_delete'
     get '/workshops/:planner_id/:workshop_id'=> 'workshops#show',as:'workshops_show' 
-  end
-  namespace :planners do
+  
     get 'posts/:workshop_id/:post_id/index'=>'posts#index',as:'posts_index'
     get '/posts/:workshop_id'=>'posts#show',as:'posts_show'
-  end
-  namespace :planners do
+
     get 'reservations/:workshop_id/index'=>'booking#index',as:'booking_index'
   end
+      
+ 
+ 
+
   namespace :users do
+      #参加者ログイン
+      get '/login_form' => 'user_sessions#new',as:'login'
+      post '/login' => 'user_sessions#create',as:'login_create'
+      delete '/logout' => 'user_sessions#destroy',as:'logout'
+       #参加者会員登録
+      get '/index' => 'users#index',as:'index'
+      get '/new'=>'users#new',as:'signup'
+      post '/create'=>'users#create',as:'create'
+      get '/:user_id/edit' => 'users#edit',as:'edit'
+      patch '/:user_id/edit'=>'users#update',as:'update'
+      get '/:user_id'=>'users#show',as:'show'
+
+      
     #レビュー
     get 'posts/index'=>'posts#index',as:'posts_index'
-    get '/:workshop_id/new'=>'posts#new',as:'posts_new'
+    get '/:user_id/posts/:workshop_id/new' => 'posts#new', as: 'posts_new'
     post '/:user_id/workshops/:workshop_id/post' => 'posts#create', as: 'workshop_post'
     get 'posts/:post_id/edit' => 'posts#edit',as:'posts_edit'
     patch 'posts/:post_id/edit'=> 'posts#update',as:'posts_update'
     get 'posts/:post_id'=>'posts#show',as:'posts_show'
+
+    #user_workshop
+    get'/workshops/index'=>'workshops#index',as:'workshops_index'
+    get '/:workshop_id/new'=>'workshops#new',as:'workshops_new'
+    get '/workshops/:u_workshop_d/edit'=>'workshops#edit',as:'workshops_edit'
+    patch '/workshops/:u_workshop_id/edit' => 'workshops#update',as:'workshops_update'
+    get '/user_id/workshops/:workshop_id'=>'workshops#show',as:'workshops_show' 
+
+    get'/:user_id/workshop/:workshop_id/reservations/new'=>'reservations#new',as:'reservations_new'
+    get '/:user_id/reservations'=>'reservations#index',as:'reservations_index'
+    post '/users/:user_id/workshops/:workshop_id/reservations' => 'reservations#create', as: 'reservations_create'
+    get '/reservations/:user_id/:reservation_id/edit'=>'reservations#edit',as:'reservations_edit'
+    patch'/reservations/:user_id/:reservation_id/edit' => 'reservations#update',as:'reservations_update'
+    delete '/reservations/:reservation_id' => 'reservations#destroy', as: 'reservations_destroy'
+    get '/:user_id/reservations/:reservation_id'=>'reservations#show',as:'reservations_show'
   end
  
-    #user_workshop
-    namespace :users do
-      get'/workshops/index'=>'workshops#index',as:'workshops_index'
-      get '/:workshop_id/new'=>'workshops#new',as:'workshops_new'
-      get '/workshops/:u_workshop_d/edit'=>'workshops#edit',as:'workshops_edit'
-      patch '/workshops/:u_workshop_id/edit' => 'workshops#update',as:'workshops_update'
-      get '/user_id/workshops/:workshop_id'=>'workshops#show',as:'workshops_show' 
-    end
-    namespace :users do
-      get'/:user_id/workshop/:workshop_id/reservations/new'=>'reservations#new',as:'reservations_new'
-      get '/:user_id/reservations'=>'reservations#index',as:'reservations_index'
-      post '/users/:user_id/workshops/:workshop_id/reservations' => 'reservations#create', as: 'reservations_create'
-      get '/reservations/:user_id/:reservation_id/edit'=>'reservations#edit',as:'reservations_edit'
-      patch'/reservations/:user_id/:reservation_id/edit' => 'reservations#update',as:'reservations_update'
-      delete '/reservations/:reservation_id' => 'reservations#destroy', as: 'reservations_destroy'
-      get '/:user_id/reservations/:reservation_id'=>'reservations#show',as:'reservations_show'
-    end
-
-  
-
-  
-
-  
-
-  
-
   get 'images/index'=>'images#index'
   get 'images/new'=>'images#new'
   post'images/create'=>'images#create', as: 'create_image'
